33a8497bd48abdf8b12fecebab176f99e3956c27
right86 <3166433427@qq.com>
2019-08-04 22:45:52 +0800

    张强   22.42,8/4

--- src/main/java/org/taru/lanqiao/dao/MenuDaoImpl.java
+++ src/main/java/org/taru/lanqiao/dao/MenuDaoImpl.java
@@ -1,6 +1,11 @@
 package org.taru.lanqiao.dao;
 
 import org.taru.lanqiao.entity.Menu;
+import org.taru.lanqiao.util.DbUtil;
+import org.taru.lanqiao.util.StringUtil;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * 菜单dao层
@@ -11,10 +16,11 @@ public class MenuDaoImpl {
      * @author
      * @return
      */
-    public Menu findById(){
-        Menu menu = null;
-
-        return menu;
+    public  List<Map<String,Object>> findById(String userId){
+        String sql="select menu_id,menu_name,menu_level,menu_sequence,menu_father_id,menu_describe,menu_url,menu_status from menus m left join role_menu rm on m.menu_id=rm.rm_menu_id left join roles r on \n" +
+                "rm.rm_role_id=r.role_id left join role_user ru on r.role_id=ru.ru_role_id left join users u on u.user_id=ru.ru_user_id where u.user_id=?";
+        List<Map<String,Object>> list= DbUtil.query(sql,userId);
+        return list;
     }
 
     /**
@@ -22,9 +28,17 @@ public class MenuDaoImpl {
      * @author
      * @return
      */
-    public int add(){
+    public int add(
+                    String menuName,
+                    String menuLevel,
+                    String menuSequence,
+                    String menuFatherId,
+                    String menuDescribe,
+                    String menuUrl,
+                    String menuStatus){
         int num = 0;
-
+        String sql="insert into menus(menu_name,menu_level,menu_sequence,menu_father_id,menu_describe,menu_url,menu_status) values(?,?,?,?,?,?,?)";
+        num=DbUtil.update(sql,menuName,menuLevel,menuSequence,menuFatherId,menuDescribe,menuUrl,menuStatus);
         return num;
     }
 
@@ -33,10 +47,11 @@ public class MenuDaoImpl {
      * @author
      * @return
      */
-    public int delete(){
-        int num = 0;
-
-        return num;
+    public int delete(String menuId){
+        int row = 0;
+        String sql="delete from menus where menu_id=?";
+        row=DbUtil.update(sql,menuId);
+        return row;
     }
 
     /**
@@ -44,9 +59,17 @@ public class MenuDaoImpl {
      * @author
      * @return
      */
-    public int update(){
-        int num = 0;
-
-        return num;
+    public int update(String menuName,
+                      String menuLevel,
+                      String menuSequence,
+                      String menuFatherId,
+                      String menuDescribe,
+                      String menuUrl,
+                      String menuStatus,
+                      String menuId){
+        int row = 0;
+        String sql="update menus set menu_name=?,menu_level=?,menu_sequence=?,menu_father_id=?,menu_describe=?,menu_url=?,menu_status=? where menu_id=?";
+        row=DbUtil.update(sql,menuName,menuLevel,menuSequence,menuFatherId,menuDescribe,menuUrl,menuStatus,menuId);
+        return row;
     }
 }
