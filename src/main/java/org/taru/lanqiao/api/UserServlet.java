866af32b4370e422eca0c579fe7694d0873326ce
jx-k <2480387023@qq.com>
2019-08-05 04:35:07 -0900

    格式 薛克

--- src/main/java/org/taru/lanqiao/api/UserServlet.java
+++ src/main/java/org/taru/lanqiao/api/UserServlet.java
@@ -14,33 +14,43 @@ import org.taru.lanqiao.vo.JsonResult;
 public class UserServlet {
     /**
      * 用户登录
-     * @author
+     *
+     * @param name
+     * @param password
      * @return
+     * @author XK
      */
     @RequestMapping("/api/user/login")
-    public JsonResult login(String name,String password){
+    public JsonResult login(String name, String password) {
         JsonResult result = null;
-        UserDaoImpl impl=new UserDaoImpl();
-        User user=impl.login(name, password);
-        if(user==null){
-            result=new JsonResult("404", "用户名或者密码错误", "");
-        }else{
-            result= new JsonResult("200", "查询用户成功", user);
+        UserDaoImpl impl = new UserDaoImpl();
+        User user = impl.login(name, password);
+        if (user == null) {
+            result = new JsonResult("404", "用户名或者密码错误", "");
+        } else {
+            result = new JsonResult("200", "查询用户成功", user);
         }
         return result;
     }
 
     /**
      * 用户注册
-     * @author
+     *
+     * @param userName
+     * @param userPassword
+     * @param userTelephone
+     * @param userPhoto
+     * @param userAddress
+     * @param userComment
      * @return
+     * @author XK
      */
     @RequestMapping("/api/user/regist")
     public JsonResult regist(String userName, String userPassword,
                              String userTelephone, String userPhoto,
-                             String userAddress, String userComment){
+                             String userAddress, String userComment) {
         JsonResult result = null;
-        User user=new User();
+        User user = new User();
         user.setUserId(IdUtil.getUuid());
         user.setUserName(userName);
         user.setUserPassword(userPassword);
@@ -48,47 +58,57 @@ public class UserServlet {
         user.setUserPhoto(userPhoto);
         user.setUserAddress(userAddress);
         user.setUserComment(userComment);
-        UserDaoImpl impl=new UserDaoImpl();
-        int i=impl.regist(user);
-        if(i==0){
-            result=new JsonResult("404","注册失败","");
-        }else{
-            result=new JsonResult("200","注册成功","");
+        UserDaoImpl impl = new UserDaoImpl();
+        int i = impl.regist(user);
+        if (i == 0) {
+            result = new JsonResult("404", "注册失败", "");
+        } else {
+            result = new JsonResult("200", "注册成功", "");
         }
         return result;
     }
 
     /**
      * 用户删除
-     * @author
+     *
+     * @param userId
      * @return
+     * @author XK
      */
     @RequestMapping("/api/user/delete")
-    public JsonResult delete(String userId){
+    public JsonResult delete(String userId) {
         JsonResult result = null;
-        UserDaoImpl impl=new UserDaoImpl();
-        int i=impl.delete(userId);
-        if(i==0){
+        UserDaoImpl impl = new UserDaoImpl();
+        int i = impl.delete(userId);
+        if (i == 0) {
             result = new JsonResult("404", "删除失败", "");
-        }else if(i==-1) {
-            result=new JsonResult("500","删除异常","");
-        }else{
-            result=new JsonResult("200","删除成功","");
+        } else if (i == -1) {
+            result = new JsonResult("500", "删除异常", "");
+        } else {
+            result = new JsonResult("200", "删除成功", "");
         }
         return result;
     }
 
     /**
      * 用户修改
-     * @author
+     *
+     * @param userId
+     * @param userName
+     * @param userPassword
+     * @param userTelephone
+     * @param userPhoto
+     * @param userAddress
+     * @param userComment
      * @return
+     * @author XK
      */
     @RequestMapping("/api/user/update")
-        public JsonResult update(String userId,String userName,String userPassword,
-                                 String userTelephone,String userPhoto,String userAddress,String userComment){
+    public JsonResult update(String userId, String userName, String userPassword,
+                             String userTelephone, String userPhoto, String userAddress, String userComment) {
         JsonResult result = null;
-        User user=new User();
-        UserDaoImpl impl=new UserDaoImpl();
+        User user = new User();
+        UserDaoImpl impl = new UserDaoImpl();
         user.setUserId(userId);
         user.setUserName(userName);
         user.setUserPassword(userPassword);
@@ -97,30 +117,32 @@ public class UserServlet {
         user.setUserAddress(userAddress);
         user.setUserComment(userComment);
         int i = impl.update(user);
-        if(i==0){
-            result=new JsonResult("404","修改失败","");
-        }else if(i==-1) {
-            result=new JsonResult("500","修改异常","");
-        }else{
-            result=new JsonResult("200","修改成功","");
+        if (i == 0) {
+            result = new JsonResult("404", "修改失败", "");
+        } else if (i == -1) {
+            result = new JsonResult("500", "修改异常", "");
+        } else {
+            result = new JsonResult("200", "修改成功", "");
         }
         return result;
     }
 
     /**
      * 根据id查询用户详情
-     * @author
+     *
+     * @param userId
      * @return
+     * @author XK
      */
     @RequestMapping("/api/user/find")
-    public JsonResult findById(String userId){
+    public JsonResult findById(String userId) {
         JsonResult result = null;
-        UserDaoImpl impl=new UserDaoImpl();
-        User user=impl.findById(userId);
-        if(user==null){
-            result=new JsonResult("404", "查询用户为空", "");
-        }else{
-            result= new JsonResult("200", "查询用户成功", user);
+        UserDaoImpl impl = new UserDaoImpl();
+        User user = impl.findById(userId);
+        if (user == null) {
+            result = new JsonResult("404", "查询用户为空", "");
+        } else {
+            result = new JsonResult("200", "查询用户成功", user);
         }
         return result;
     }
--- src/main/java/org/taru/lanqiao/dao/UserDaoImpl.java
+++ src/main/java/org/taru/lanqiao/dao/UserDaoImpl.java
@@ -13,89 +13,100 @@ import java.util.Map;
 public class UserDaoImpl {
     /**
      * 用户登录
-     * @author
+     *
+     * @param name
+     * @param password
      * @return
+     * @author XK
      */
-    public User login(String name,String password){
+    public User login(String name, String password) {
         User user = null;
-        String sql="select user_id,user_name,user_password,user_telephone,user_photo,user_comment,user_status from users where user_name=?";
-            try {
-                user = new User();
-                List<Map<String, Object>> list = DbUtil.query(sql, name);
-                if (list.size() == 0||!password.equals(list.get(0).get("user_password"))) {
-                    user=null;
-                } else {
-                    user.setUserId(StringUtil.valueOf(list.get(0).get("user_id")));
-                    user.setUserName(StringUtil.valueOf(list.get(0).get("user_name")));
-                    user.setUserTelephone(StringUtil.valueOf(list.get(0).get("user_telephone")));
-                    user.setUserPhoto(StringUtil.valueOf(list.get(0).get("user_photo")));
-                    user.setUserComment(StringUtil.valueOf(list.get(0).get("user_comment")));
-                    user.setUserStatus(StringUtil.valueOf( list.get(0).get("user_status")));
-                }
-            }catch (Exception ex){
-                user=null;
+        String sql = "select user_id,user_name,user_password,user_telephone,user_photo,user_comment,user_status from users where user_name=?";
+        try {
+            user = new User();
+            List<Map<String, Object>> list = DbUtil.query(sql, name);
+            if (list.size() == 0 || !password.equals(list.get(0).get("user_password"))) {
+                user = null;
+            } else {
+                user.setUserId(StringUtil.valueOf(list.get(0).get("user_id")));
+                user.setUserName(StringUtil.valueOf(list.get(0).get("user_name")));
+                user.setUserTelephone(StringUtil.valueOf(list.get(0).get("user_telephone")));
+                user.setUserPhoto(StringUtil.valueOf(list.get(0).get("user_photo")));
+                user.setUserComment(StringUtil.valueOf(list.get(0).get("user_comment")));
+                user.setUserStatus(StringUtil.valueOf(list.get(0).get("user_status")));
             }
+        } catch (Exception ex) {
+            user = null;
+        }
         return user;
     }
 
     /**
      * 用户注册
-     * @author
+     *
+     * @param user
      * @return
+     * @author XK
      */
-    public int regist(User user){
+    public int regist(User user) {
         int num = 0;
-        String sql="INSERT INTO users (user_id,user_name,user_password,user_telephone,user_photo,user_address,user_comment) VALUES (?,?,?,?,?,?,?)";
-        try{
-            num= DbUtil.update(sql,user.getUserId(),user.getUserName(),user.getUserPassword(),user.getUserTelephone(),user.getUserPhoto(),user.getUserAddress(),user.getUserComment());
-            }catch (Exception ex){
-            num=-1;
+        String sql = "INSERT INTO users (user_id,user_name,user_password,user_telephone,user_photo,user_address,user_comment) VALUES (?,?,?,?,?,?,?)";
+        try {
+            num = DbUtil.update(sql, user.getUserId(), user.getUserName(), user.getUserPassword(), user.getUserTelephone(), user.getUserPhoto(), user.getUserAddress(), user.getUserComment());
+        } catch (Exception ex) {
+            num = -1;
         }
         return num;
     }
 
     /**
      * 用户删除
-     * @author
+     *
+     * @param userId
      * @return
+     * @author XK
      */
-    public int delete(String userId){
+    public int delete(String userId) {
         int num = 0;
-        String sql="update users set user_status=0 where user_id=?";
+        String sql = "update users set user_status=0 where user_id=?";
         try {
-            num=DbUtil.update(sql,userId);
-        }catch (Exception ex){
-            num=0;
+            num = DbUtil.update(sql, userId);
+        } catch (Exception ex) {
+            num = 0;
         }
         return num;
     }
 
     /**
      * 用户修改
-     * @author
+     *
+     * @param user
      * @return
+     * @author XK
      */
-    public int update(User user){
+    public int update(User user) {
         int num = 0;
-        String sql="update users set user_name=?,user_password=?,user_telephone=?,user_photo=?,user_address=?,user_comment=? where user_id=?";
+        String sql = "update users set user_name=?,user_password=?,user_telephone=?,user_photo=?,user_address=?,user_comment=? where user_id=?";
         try {
-            num= DbUtil.update(sql,user.getUserName(),user.getUserPassword(),user.getUserTelephone(),user.getUserPhoto(),user.getUserAddress(),user.getUserComment(),user.getUserId());
-        }catch (Exception ex){
-            num=0;
+            num = DbUtil.update(sql, user.getUserName(), user.getUserPassword(), user.getUserTelephone(), user.getUserPhoto(), user.getUserAddress(), user.getUserComment(), user.getUserId());
+        } catch (Exception ex) {
+            num = 0;
         }
         return num;
     }
 
     /**
      * 根据id查询用户详情
-     * @author
+     *
+     * @param userId
      * @return
+     * @author XK
      */
-    public User findById(String userId){
+    public User findById(String userId) {
         User user = null;
-        String sql="select user_id,user_name,user_telephone,user_photo,user_comment,user_status from users where user_id=?";
+        String sql = "select user_id,user_name,user_telephone,user_photo,user_comment,user_status from users where user_id=?";
         List<Map<String, Object>> list = DbUtil.query(sql, userId);
-        try{
+        try {
             if (list.size() == 0) {
             } else {
                 user = new User();
@@ -104,10 +115,10 @@ public class UserDaoImpl {
                 user.setUserTelephone(StringUtil.valueOf(list.get(0).get("user_telephone")));
                 user.setUserPhoto(StringUtil.valueOf(list.get(0).get("user_photo")));
                 user.setUserComment(StringUtil.valueOf(list.get(0).get("user_comment")));
-                user.setUserStatus(StringUtil.valueOf( list.get(0).get("user_status")));
+                user.setUserStatus(StringUtil.valueOf(list.get(0).get("user_status")));
             }
-        }catch(Exception ex){
-            user=null;
+        } catch (Exception ex) {
+            user = null;
         }
         return user;
     }
