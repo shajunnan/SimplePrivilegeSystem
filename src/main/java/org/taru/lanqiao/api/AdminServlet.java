c25389ccbf5730d236b144751d6ecd69db38b494
trnanks <591716377@qq.com>
2019-08-05 23:05:17 +0800

    完成管理员基础查询功能

--- src/main/java/org/taru/lanqiao/api/AdminServlet.java
+++ src/main/java/org/taru/lanqiao/api/AdminServlet.java
@@ -1,7 +1,10 @@
 package org.taru.lanqiao.api;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.taru.lanqiao.dao.AdminDaoImpl;
+import org.taru.lanqiao.entity.Admin;
 import org.taru.lanqiao.vo.JsonResult;
 
 import java.util.List;
@@ -11,27 +14,52 @@ import java.util.List;
  */
 @RestController
 public class AdminServlet {
-
     /**
      * 管理员登录
-     * @author
+     * @author ShaJunnan
+     * @param name
+     * @param password
      * @return
      */
     @RequestMapping("/admin/login")
-    public JsonResult login(){
+    public JsonResult login(@RequestParam("name")String name,@RequestParam("password")String password){
         JsonResult result = null;
+
+        try {
+            AdminDaoImpl impl = new AdminDaoImpl();
+            Admin admin = impl.login(name,password);
+            if(admin != null){
+                result = new JsonResult("200","管理员登陆成功",admin);
+            }else {
+                result = new JsonResult("404","管理员登陆失败,账号或密码错误",null);
+            }
+        }catch (Exception e){
+            result = new JsonResult("500","管理员登陆异常",null);
+        }
 
         return result;
     }
 
     /**
      * 管理员列表
-     * @author
+     * @author ShaJunnan
      * @return
      */
     @RequestMapping("/admin/list")
     public JsonResult queryList(){
         JsonResult result = null;
+
+        try {
+            AdminDaoImpl impl = new AdminDaoImpl();
+            List<Admin> adminList = impl.queryList();
+            if(adminList != null && adminList.size() > 0){
+                result = new JsonResult("200","查询管理员列表成功",adminList);
+            }else {
+                result = new JsonResult("404","查询管理员列表失败",null);
+            }
+        }catch (Exception e){
+            result = new JsonResult("500","查询管理员列表异常",null);
+        }
 
         return result;
     }
@@ -39,12 +67,25 @@ public class AdminServlet {
 
     /**
      * 管理员详情
-     * @author
+     * @author ShaJunnan
+     * @param id
      * @return
      */
     @RequestMapping("/admin/find")
-    public JsonResult findById(){
+    public JsonResult findById(@RequestParam("id") String id){
         JsonResult result = null;
+
+        try {
+            AdminDaoImpl impl = new AdminDaoImpl();
+            Admin admin = impl.findById(id);
+            if(admin != null){
+                result = new JsonResult("200","查询管理员详情成功",admin);
+            }else{
+                result = new JsonResult("404","查询管理员详情失败",null);
+            }
+        }catch (Exception e){
+            result = new JsonResult("500","查询管理员详情异常",null);
+        }
 
         return result;
     }
--- src/main/java/org/taru/lanqiao/dao/AdminDaoImpl.java
+++ src/main/java/org/taru/lanqiao/dao/AdminDaoImpl.java
@@ -1,8 +1,12 @@
 package org.taru.lanqiao.dao;
 
 import org.taru.lanqiao.entity.Admin;
+import org.taru.lanqiao.util.DbUtil;
+import org.taru.lanqiao.util.StringUtil;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 管理员dao层
@@ -13,33 +17,77 @@ public class AdminDaoImpl {
      * @author
      * @return
      */
-    public Admin login(){
+    public Admin login(String name,String password){
+        String sql = "SELECT * FROM admins WHERE admin_name = ?  AND admin_password = ?";
+        List<Map<String,Object>> dataList = DbUtil.query(sql,name,password);
         Admin admin = null;
+        if(dataList != null && dataList.size() > 0){
+            Map<String,Object> map = dataList.get(0);
+            admin = new Admin();
+            admin.setAdminId(StringUtil.valueOf(map.get("admin_id")));
+            admin.setAdminName(StringUtil.valueOf(map.get("admin_name")));
+            admin.setAdminTelephone(StringUtil.valueOf(map.get("admin_telephone")));
+            admin.setAdminPhoto(StringUtil.valueOf(map.get("admin_photo")));
+            admin.setAdminComment(StringUtil.valueOf(map.get("admin_comment")));
+            admin.setAdminStatus(StringUtil.valueOf(map.get("admin_status")));
+        }
 
+        DbUtil.close();
         return admin;
     }
 
     /**
      * 管理员列表
-     *
-     * @return
+     * @return ShaJunnan
      * @author
      */
     public List<Admin> queryList() {
+        String sql = "SELECT * FROM admins";
+        List<Map<String,Object>> dataList = DbUtil.query(sql);
         List<Admin> resultList = null;
+        if(dataList != null && dataList.size() > 0){
+            resultList = new ArrayList<Admin>();
+            for(Map<String,Object> map:dataList){
+                Admin admin = new Admin();
+                admin.setAdminId(StringUtil.valueOf(map.get("admin_id")));
+                admin.setAdminName(StringUtil.valueOf(map.get("admin_name")));
+                admin.setAdminPassword(StringUtil.valueOf(map.get("admin_password")));
+                admin.setAdminTelephone(StringUtil.valueOf(map.get("admin_telephone")));
+                admin.setAdminPhoto(StringUtil.valueOf(map.get("admin_photo")));
+                admin.setAdminComment(StringUtil.valueOf(map.get("admin_comment")));
+                admin.setAdminStatus(StringUtil.valueOf(map.get("admin_status")));
 
+                resultList.add(admin);
+            }
+        }
+
+        DbUtil.close();
         return resultList;
     }
 
 
     /**
      * 管理员详情
-     * @author
+     * @author ShaJunnan
+     * @param id
      * @return
      */
-    public Admin findById(){
+    public Admin findById(String id){
+        String sql = "SELECT * FROM admins WHERE admin_id = ?";
+        List<Map<String,Object>> dataList = DbUtil.query(sql,id);
         Admin admin = null;
+        if(dataList != null && dataList.size() > 0){
+            Map<String,Object> map = dataList.get(0);
+            admin = new Admin();
+            admin.setAdminId(StringUtil.valueOf(map.get("admin_id")));
+            admin.setAdminName(StringUtil.valueOf(map.get("admin_name")));
+            admin.setAdminTelephone(StringUtil.valueOf(map.get("admin_telephone")));
+            admin.setAdminPhoto(StringUtil.valueOf(map.get("admin_photo")));
+            admin.setAdminComment(StringUtil.valueOf(map.get("admin_comment")));
+            admin.setAdminStatus(StringUtil.valueOf(map.get("admin_status")));
+        }
 
+        DbUtil.close();
         return admin;
     }
 }
