7de4581ea8e85098a30c8a857cd1a2edaafd7677
trnanks <591716377@qq.com>
2019-08-06 23:54:18 +0800

    完成登录验证过滤器Cookie

--- src/main/java/org/taru/lanqiao/filter/LoginValidFilter.java
+++ src/main/java/org/taru/lanqiao/filter/LoginValidFilter.java
@@ -1,11 +1,14 @@
 package org.taru.lanqiao.filter;
 
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.taru.lanqiao.entity.User;
 import org.taru.lanqiao.util.JsonWriter;
+import org.taru.lanqiao.util.SecurityUtil;
 import org.taru.lanqiao.vo.JsonResult;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -13,10 +16,38 @@ import java.io.IOException;
 public class LoginValidFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
-        JsonResult result = null;
-        /*result = new JsonResult("500","未登录，无访问权限",null);
-        JsonWriter.writer(response,result);*/
-        chain.doFilter(request,response);   // 放行
+        // 如果是登录，就放行
+        String path = request.getRequestURI();
+        if("/api/user/login".equals(path)) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        // 获取请求的cookie对比令牌
+        Cookie[] cookies = request.getCookies();
+        if(cookies != null && cookies.length > 0) {
+            for(Cookie ck : cookies) {
+                if(ck.getName().equals("token")) {
+                    if(ck.getValue().equals(SecurityUtil.encryptionMd5("lanqiao"))) {
+                        chain.doFilter(request, response);	// 放行
+                        return;
+                    }
+                }
+            }
+        }
+
+        JsonResult result = new JsonResult("500","未登录，无访问权限",null);
+        JsonWriter.writer(response,result);
+
+        // 当前登录用户,使用session对象，判断是否携带userId
+        /*User user = (User)request.getSession().getAttribute("loginUserKey");
+        // 如果已登录则放行，否则返回未登录
+        if(user != null){
+            chain.doFilter(request,response);   // 放行
+        }else{
+            JsonResult result = new JsonResult("500","未登录，无访问权限",null);
+            JsonWriter.writer(response,result);
+        }*/
     }
 
     /**
